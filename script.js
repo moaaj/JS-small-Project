c4e869e4c29c995ddc84f5f1c29dbc7ffec38d69
Abdullah Moaaj <67410953+moaaj@users.noreply.github.com>
2024-06-04 23:48:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+ var base = parseFloat(prompt("Enter Base ")) ;
+ var height = parseFloat(prompt("Enter Height "));
+
+
+ var area = base * height;
+
+ document.write("Area of rectangle = " + area + "<br/>");
+
+
+ var area2 = (base * height)/2;
+
+ document.write("Area of Triangle = " + area2 );
