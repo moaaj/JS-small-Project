db6e2300eca291abf1b0e98fd9031854fd563204
Abdullah Moaaj <67410953+moaaj@users.noreply.github.com>
2024-06-04 23:35:49 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var num1 = prompt("Enter First Number: ")
+        var num2 = prompt("Enter Second Number: ")
+
+
+        num1 = parseFloat(num1, 10);
+        num2 = parseFloat(num2, 10);
+
+        var sum,sub,mul,div,rem;
+
+        sum = num1 + num2;
+        document.write(num1 + " + " + num2 + " = "  + sum + "<br/>");
+
+        sub = num1 - num2;
+        document.write(num1 + " - " + num2 + " = "  + sub + "<br/>");
+
+        mul = num1 * num2;
+        document.write(num1 + " * " + num2 + " = "  + mul + "<br/>");
+
+        div = num1 / num2;
+        document.write(num1 + " / " + num2 + " = "  + div + "<br/>");
+
+        rem = num1 % num2;
+        document.write(num1 + " % " + num2 + " = "  + rem + "<br/>");
+        
