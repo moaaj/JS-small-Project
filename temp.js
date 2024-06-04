e87c9a9204a6863188997f482e715a4f5a9b04c9
Abdullah Moaaj <67410953+moaaj@users.noreply.github.com>
2024-06-04 23:57:33 +0800

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,13 @@
+var farn = parseFloat(prompt("Enter Farhenheit"));
+
+var cels = (farn - 32) *  (5/9);
+
+document.write("Celsius = " + cels + "<br/>");
+
+var cels = parseFloat(prompt("Enter Celsius"));
+
+var farn = (cels * (9/5)) + 32;
+document.write("Ferhenheit = " + farn);
+
+
+
