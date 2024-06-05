16780ba60d8eb7f0490cf6f666eb7f49ddc67a4e
Abdullah Moaaj <67410953+moaaj@users.noreply.github.com>
2024-06-05 19:21:11 +0800

    Add files via upload

---
+++ Loop2.js
@@ -0,0 +1,7 @@
+
+for (var x = 1; x <=5; x++){
+    var num1 = parseInt(prompt("Enter first number"));
+    var num2 = parseInt(prompt("Enter Second Number: "));
+    var sum = num1 + num2;
+    console.log("Result = " + sum);
+}
