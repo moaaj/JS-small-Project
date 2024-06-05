25ac9067f5a4d013d74f8be86c32ae5795bc10ea
Abdullah Moaaj <67410953+moaaj@users.noreply.github.com>
2024-06-05 19:46:45 +0800

    Add files via upload

---
+++ program.js
@@ -0,0 +1,17 @@
+
+
+var i = 1;
+var sum = 0;
+
+while(i <= 50){
+
+    if(i%3==0 && i%5==0){
+        document.write(" " + i);
+        sum = sum + i;
+
+    }
+    
+    i = i + 1;
+}
+document.write(" Sum =  " + sum);
+document.write("<h1>End</h1>")
