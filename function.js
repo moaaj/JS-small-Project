5ca06b651e17b9aede84af47f64999e5c4e90fbe
Abdullah Moaaj <67410953+moaaj@users.noreply.github.com>
2024-06-08 21:51:43 +0800

    Add files via upload

---
+++ function.js
@@ -0,0 +1,55 @@
+
+
+
+
+// add function
+
+function addition(x,y){
+
+    var result = x+y;
+    document.write("Sum = " + result + "<br>");
+
+}
+
+
+// sub function
+
+
+function subtraction(x,y){
+
+    var result = x-y;
+    document.write("Sub = " + result + "<br>");
+
+}
+
+
+
+
+
+
+
+//Creating a function
+
+function square(num1, num2) {
+var result = num1 * num2;
+return result;
+}
+
+//calling a function
+
+var x = square(5,6);
+document.write(x);
+
+
+addition(10,20);
+addition(580,60);
+subtraction(30,20);
+
+
+//creating a function
+//calling a function
+// how to pass value in function
+// returning a value
+
+
+
